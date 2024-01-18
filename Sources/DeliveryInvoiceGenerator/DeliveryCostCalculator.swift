6e8418c636855e2469ec3d1e66b1564c3eb5515b
Javal Nanda <>
2024-01-18 20:44:42 +0530

    Consider offer in calculation

--- Sources/DeliveryInvoiceGenerator/DeliveryCostCalculator.swift
+++ Sources/DeliveryInvoiceGenerator/DeliveryCostCalculator.swift
@@ -1,5 +1,66 @@
+struct Offer {
+    let code: String
+    let discountInPercentage: Double
+    let criteria: Criteria
+
+    struct Criteria {
+        let distance: Range
+        let weight: Range
+    }
+
+    struct Range {
+        let min: Double
+        let max: Double
+    }
+}
+
 struct DeliveryCostCalculator {
+    private let offers: [Offer] = [
+        Offer(
+            code: "OFR001",
+            discountInPercentage: 10,
+            criteria: Offer.Criteria(
+                distance: Offer.Range(min: 0, max: 199),
+                weight: Offer.Range(min: 70, max: 200)
+            )
+        ),
+        Offer(
+            code: "OFR002",
+            discountInPercentage: 7,
+            criteria: Offer.Criteria(
+                distance: Offer.Range(min: 50, max: 150),
+                weight: Offer.Range(min: 100, max: 250)
+            )
+        ),
+        Offer(
+            code: "OFR003",
+            discountInPercentage: 5,
+            criteria: Offer.Criteria(
+                distance: Offer.Range(min: 50, max: 250),
+                weight: Offer.Range(min: 10, max: 150)
+            )
+        ),
+    ]
+
     func calculateTotalEstimatedCost(baseDeliveryCost: Double, package: Package) -> Double {
-        return baseDeliveryCost + (package.weightInKg * 10) + (package.distanceToDestination * 5)
+        let deliveryCostWithoutDiscount = baseDeliveryCost + (package.weightInKg * 10) + (package.distanceToDestination * 5)
+        let discountPercentage = discountPercentageForPackage(package: package)
+        let discountToApply = deliveryCostWithoutDiscount * (discountPercentage/100)
+        let totalEstimatedPrice = deliveryCostWithoutDiscount - discountToApply
+        return totalEstimatedPrice
+    }
+
+    private func discountPercentageForPackage(package: Package) -> Double {
+        guard let validOffer = offers.first(where: { $0.code == package.offerCode }) else {
+            return 0.0
+        }
+
+        if package.distanceToDestination >= validOffer.criteria.distance.min && 
+            package.distanceToDestination <= validOffer.criteria.distance.max &&
+            package.weightInKg >= validOffer.criteria.weight.min &&
+            package.weightInKg <= validOffer.criteria.weight.max {
+            return validOffer.discountInPercentage
+        }
+        return 0
     }
 }
--- Tests/DeliveryInvoiceGenerator/DeliveryCostCalulatorTests.swift
+++ Tests/DeliveryInvoiceGenerator/DeliveryCostCalulatorTests.swift
@@ -13,4 +13,26 @@ final class DeliveryCostCalulatorTests: XCTestCase {
         let expectedEstimatedCost = 175.0
         XCTAssertEqual(expectedEstimatedCost, totalEstimatedCost)
     }
+
+    func test_totalEstimatedCostWithValidOfferCriteria_returns_totalCost_afterApplyingDiscount() {
+        let sut = DeliveryCostCalculator()
+
+        let baseDeliveryCost = 100.0
+        let package = Package(id: "PKG3", weightInKg: 10.0, distanceToDestination: 100.0, offerCode: "OFR003")
+        let totalEstimatedCost = sut.calculateTotalEstimatedCost(baseDeliveryCost: baseDeliveryCost,package: package)
+
+        let expectedEstimatedCost = 665.0
+        XCTAssertEqual(expectedEstimatedCost, totalEstimatedCost)
+    }
+
+    func test_totalEstimatedCostWithValidOfferCode_notMeetingOfferCriteria_returns_totalCost_withoutDiscount() {
+        let sut = DeliveryCostCalculator()
+
+        let baseDeliveryCost = 100.0
+        let package = Package(id: "PKG1", weightInKg: 5.0, distanceToDestination: 5.0, offerCode: "OFR001")
+        let totalEstimatedCost = sut.calculateTotalEstimatedCost(baseDeliveryCost: baseDeliveryCost,package: package)
+
+        let expectedEstimatedCost = 175.0
+        XCTAssertEqual(expectedEstimatedCost, totalEstimatedCost)
+    }
 }
